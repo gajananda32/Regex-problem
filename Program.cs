c400385971ebbbdc0a321bb30a16908d0d2f2fdf
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-03 17:00:12 +0530

    Added Rule4

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using RegexUC8;
+
+Pattern p = new Pattern();
+Console.WriteLine("Enter Pattern");
+string password = Console.ReadLine();
+bool value = p.validiate(password);
+if (value)
+{
+    Console.WriteLine("Pattern is Matched");
+}
+else
+{
+    Console.WriteLine("Pattern is not Matched");
+}
+
