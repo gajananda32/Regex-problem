63fe375761d92ad5056d0c5d455ed5acfbded00b
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-03 16:51:37 +0530

    Added PhoneNumber

---
+++ Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPhoneNumber
+{
+    public class Pattern
+    {
+        public static string Regex_PhoneNumber = "^(91)?[' ']{1}[6-9]{1}[0-9]{9}$";
+        public bool validatePincode(string PhoneNumber)
+        {
+            return Regex.IsMatch(PhoneNumber, Regex_PhoneNumber);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//creating object 
+//taking input from user
+using RegexPhoneNumber;
+
+Pattern p = new Pattern();
+Console.WriteLine("Enter the pattern ");
+string ph = Console.ReadLine();
+bool value = p.validatePincode(ph);
+if (value)
+{
+    Console.WriteLine("Pattern is matched ");
+}
+else
+{
+    Console.WriteLine("Pattern is not matched ");
+}
