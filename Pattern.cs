e2e1a59fb1460349821045dd9e12c50686ba72c9
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-03 16:46:59 +0530

    Added FirstName

---
+++ Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexxUC1
+{
+    public class Pattern
+    {
+        //creating regex
+        public static string Regex_FirstName = "^[A-Z]{1}[a-z]{3,}$";
+        public bool validatePincode(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, Regex_FirstName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using RegexxUC1;
+//creating object 
+//taking input from user
+Pattern p = new Pattern();
+Console.WriteLine("Enter the pattern ");
+string FirstName = Console.ReadLine();
+bool value = p.validatePincode(FirstName);
+if (value)
+{
+    Console.WriteLine("Pattern is matched ");
+}
+else
+{
+    Console.WriteLine("Pattern is not matched ");
+}
