82c134889c03d99102c8f1ccbc5ac517156d502b
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-03 16:54:35 +0530

    Added Rule2

---
+++ Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUC6
+{
+    public class Pattern
+    {
+        public string Regex_PasswordRule2 = "^([A-Z]+)[a-z]{8,15}$";
+        public bool Validiate(string password)
+        {
+            return Regex.IsMatch(password, Regex_PasswordRule2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using RegexUC6;
+
+Pattern p = new Pattern();
+Console.WriteLine("Enter Pattern");
+string password = Console.ReadLine();
+bool value = p.Validiate(password);
+if (value)
+{
+    Console.WriteLine("Pattern is Matched");
+}
+else
+{
+    Console.WriteLine("Pattern is not Matched");
+}
