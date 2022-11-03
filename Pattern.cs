b217e48bc374adbbf5f0bb87b0169a7127318cbb
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-03 16:55:54 +0530

    Added Rule7

---
+++ Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUC7
+{
+    public class Pattern
+    {
+        public string Rergex_PosswordRule3 = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,15}$";
+        public bool validiate(string password)
+        {
+            return Regex.IsMatch(password, Rergex_PosswordRule3);
+        }
+    }
+}
