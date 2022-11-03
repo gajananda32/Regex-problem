91f5420b7c465da7d88716688920531639d6404c
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-03 16:52:54 +0530

    PAssword Rule1

---
+++ Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUC5
+{
+    public class Pattern
+    {
+        public string Regex_Password = "^[a-z]{8,13}$";
+        public bool ValidateRegex(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using RegexUC5;
+
+Pattern p = new Pattern();
+Console.WriteLine("enter the pattern");
+string password= Console.ReadLine();
+bool value = p.ValidateRegex(password);
+if (value)
+{
+    Console.WriteLine("Pattern is Matched");
+}
+else
+{
+    Console.WriteLine("Pattern is Not Matched");
+}
