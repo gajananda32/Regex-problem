f7343b1579b720559c1fd5223d358af3f27ed313
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-03 16:50:11 +0530

    Added Email

---
+++ Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUC3
+{
+    public class Pattern
+    {
+        //creating regex
+        public static string Regex_Email = "^[a-z]{4,}[0-9]{2,4}['@']{1}[a-z]{2,6}['.'][a-z]{2,6}$";
+        public bool validatePincode(string Email)
+        {
+            return Regex.IsMatch(Email, Regex_Email);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using RegexUC3;
+//creating object 
+//taking input from user
+Pattern p = new Pattern();
+Console.WriteLine("Enter the pattern ");
+string Email = Console.ReadLine();
+bool value = p.validatePincode(Email);
+if (value)
+{
+    Console.WriteLine("Pattern is matched ");
+}
+else
+{
+    Console.WriteLine("Pattern is not matched ");
+}
