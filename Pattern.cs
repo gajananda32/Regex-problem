85cb6753b34193e913732c2dc56ab7acf510d43d
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-11-03 16:48:12 +0530

    LastName

---
+++ Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUC2
+{
+    public  class Pattern
+    {
+        //creating regex
+        public static string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool validatePincode(string LastName)
+        {
+            return Regex.IsMatch(LastName, Regex_LastName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//creating object 
+//taking input from user
+using RegexUC2;
+
+Pattern p = new Pattern();
+Console.WriteLine("Enter the pattern ");
+string LastName = Console.ReadLine();
+bool value = p.validatePincode(LastName);
+if (value)
+{
+    Console.WriteLine("Pattern is matched ");
+}
+else
+{
+    Console.WriteLine("Pattern is not matched ");
+}
